98e7a00870cb36d0b925e013e7606a0c4f639c38
rhgusdn0708 <hu070821@gmail.com>
2025-05-23 09:36:48 +0900

    firstcommit

---
+++ src/components/TodoItemList.jsx
@@ -0,0 +1,40 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+class TodoItemList extends Component {
+    /*
+        true() 이면 render()함수가 호출됨됨
+    */
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.myTodos !== nextProps.myTodos;
+    }
+
+    render() {
+        const { myTodos, myToggle, myRemove } = this.props;
+        const todoList = myTodos.map(
+            ({ id, text, checked }) => (
+                <TodoItem
+                    id={id}
+                    text={text}
+                    checked={checked}
+                    onToggle={myToggle}
+                    onRemove={myRemove}
+                    key={id}
+                />
+            )
+        );
+        return (
+            <div>
+                {todoList}
+            </div>
+        );
+    }
+}
+
+TodoItemList.propTypes = {
+    myTodos: PropTypes.array,
+    myToggle: PropTypes.func,
+    myRemove: PropTypes.func
+};
+export default TodoItemList;
